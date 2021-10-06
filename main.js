b040910e7b70fd17de35e3c75f1ae1e90f20b802
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 10:11:37 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+//To genereate a number between 0-1
+Math.random();
+//To generate a number that is a whole number rounded down
+Math.floor(Math.random())
+/*To generate a number that is a whole number rounded down between
+1 and 10 */
+Math.floor(Math.random() * 10) + 1 //the + 1 makes it so its not 0.
